cca5e31cb708de66200b8d018f7231064107d85a
dmycse <dmycse@gmail.com>
2024-12-24 17:41:52 +0200

    Feat(components): create app pages with routing

---
+++ src/components/routes/index.js
@@ -0,0 +1,6 @@
+export { Homepage } from './Homepage';
+export { PostListPage } from './PostListPage';
+export { SinglePostPage } from './SinglePostPage';
+export { Write } from './Write';
+export { LoginPage } from './LoginPage';
+export { RegisterPage } from './RegisterPage';
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Homepage, PostListPage, SinglePostPage, Write, LoginPage, RegisterPage } from './components/routes';
 import './index.css'
-import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/posts",
+      element: <PostListPage />,
+    },
+    {
+      path: "/:slug",
+      element: <SinglePostPage />,
+    },
+    {
+      path: "/write",
+      element: <Write />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+ 
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
