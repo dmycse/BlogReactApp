c4b927c199c2fff6055ef48d30c8730f9f11d3be
dmycse <dmycse@gmail.com>
2024-12-24 19:34:37 +0200

    Refactor(components): refactor login page

--- src/components/routes/LoginPage.jsx
+++ src/components/routes/LoginPage.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import { SignIn } from '@clerk/clerk-react';
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='h-[calc(100vh-4rem)] flex items-center justify-center'>
+      <SignIn signUpUrl='/register' />
+    </div>
   )
 }
