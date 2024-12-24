7958d5907f3ef8c4fadeb9499e99fbd7493df02e
dmycse <dmycse@gmail.com>
2024-12-24 19:33:31 +0200

    Refactor(layout): refactor main layout

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Navbar } from "../navbar";
 
 
-export const MainLayout = ({children}) => {
+export const MainLayout = () => {
   return (
     <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
       <Navbar />
