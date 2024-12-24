3b72c16391180dd0a606a4ed536e756d284cc815
dmycse <dmycse@gmail.com>
2024-12-24 19:35:57 +0200

    Feat(routes): feat register page

--- src/components/routes/RegisterPage.jsx
+++ src/components/routes/RegisterPage.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import { SignUp } from "@clerk/clerk-react";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='h-[calc(100vh-4rem)] flex items-center justify-center'>
+      <SignUp signInUrl='/login' />
+    </div>
   )
 }
