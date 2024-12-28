b7be9513cfeee04a60470162b81759d4f1e4cbbc
dmycse <dmycse@gmail.com>
2024-12-28 22:05:30 +0200

    Feat(routes): feat post route by add api endpoints

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { getAllPosts, getPost, createPost, deletePost } from "../controllers/post.controller.js";
+
 
 const router = Router();
 
+router.get("/", getAllPosts);
+router.get("/:slug", getPost);
+
+router.post("/", createPost);
+
+router.delete("/:id", deletePost);
 
 
 export default router;
