c2464cc1dfc6eba7c5d51333edfa33fcb73c4a71
dmycse <dmycse@gmail.com>
2024-12-29 18:15:46 +0200

    Feat(controllers post): feat post contoller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
 
 // In Espress v.5 you don't need to use try/catch cause 
 // if an error is thrown it will be catched error automatically
@@ -16,13 +17,42 @@ export const getPost = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const newPost = new Post({...req.body});
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const newPost = new Post({user: user._id, ...req.body});
 
   const post = await newPost.save();
   res.status(200).json(post);
 };
 
 export const deletePost = async (req, res) => {
-  const post = await Post.findByIdAndDelete(req.params.id);
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  const deletedPost = await Post.findByIdAndDelete({
+    _id: req.params.id, 
+    user: user._id
+  });
+
+
+  if (!deletedPost) {
+    return res.status(403).json({ message: "You can't delete this post" });
+  }
+
   res.status(200).json('Post deleted');
 };
