0979f331ca27623b65dc9e0088e0dea68ab90fbd
dmycse <dmycse@gmail.com>
2024-12-29 17:30:00 +0200

    Feat index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,36 @@
 import express from "express";
+import connectDB from "./lib/connectDB.js";
+
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
+
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.status(200).send("It works!");
-})
+app.use("/webhooks", webhookRouter);
 
-app.listen(3000, () => {
-  console.log('\x1b[36m%s\x1b[0m', "Server is running. Port: 3000");
+app.use(express.json());
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+// Error handler (for Express.js v.5+)
+app.use((err, req, res, next) => {
+  console.log(err);
+  res
+    .status(err.status || 500)
+    .json(
+      { 
+        message: err.message || "Something went wrong",
+        status: err.status || 500
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log('\x1b[36m%s\x1b[0m', `Server is running. Port: ${PORT}`);
 });
