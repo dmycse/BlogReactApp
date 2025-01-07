c094ab25d44f3a44792d07ee8afcf1aed9af0129
dmycse <dmycse@gmail.com>
2025-01-07 11:58:34 +0200

    Feat(components sections): add react query in postslist component

--- frontend/src/components/sections/PostsList.jsx
+++ frontend/src/components/sections/PostsList.jsx
@@ -1,7 +1,16 @@
 import { PostItem } from "../posts";
+import { useQuery } from '@tanstack/react-query';
+import { fetchPosts } from "../../lib/fetchPosts";
 import { postsList } from "../../constants/constants";
 
 export const PostsList = () => {
+
+  const { isPending, data, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+
+
   return (
     <div className='mb-8 flex flex-col gap-4'>
       {postsList.map(item => (
