72b699965730a7b8248373b3f92c7a9792c87bf3
dmycse <dmycse@gmail.com>
2025-01-07 11:52:56 +0200

    Feat(lib fetchposts):  add fetchpost func and add token authorization in createpost func

--- frontend/src/lib/fetchPosts.js
+++ frontend/src/lib/fetchPosts.js
@@ -1,13 +1,20 @@
+
 export const fetchPosts = async () => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/posts`);
   return await res.json();
 };
 
-export const  createPost = async (post) => {
+export const fetchPost = async (slug) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/posts/${slug}`);
+  return await res.json();
+};
+
+export const createPost = async (post, token) => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(post),
   });
