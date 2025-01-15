bdcaa315e907cba9f4b5adb2452ad48e6b438082
dmycse <dmycse@gmail.com>
2025-01-15 10:55:54 +0200

    View: create CreatePostPage component

---
+++ frontend/src/view/createPostPage/ui/CreatePostPage.jsx
@@ -0,0 +1,11 @@
+import { CreatePostForm } from "@/features/create-post";
+
+export const CreatePostPage = () => {
+
+  return (
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
+      <h1 className="text-cl font-light">Create a New Post</h1>
+      <CreatePostForm />
+    </div>
+  )
+}
