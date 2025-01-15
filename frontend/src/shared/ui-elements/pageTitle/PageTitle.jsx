1a42a3fa2d43b4484d032c9a394f67a32815dc52
dmycse <dmycse@gmail.com>
2025-01-15 11:15:20 +0200

    Shared: create PageTitle component

---
+++ frontend/src/shared/ui-elements/pageTitle/PageTitle.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const PageTitle = () => {
+  return (
+    <div>
+      <h1 className='text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing.
+      </h1>
+    <p className='mt-8 text-md md:text-xl'>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+      Eos voluptate consectetur non dolorum.
+    </p>
+  </div>
+  )
+};
