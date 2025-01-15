0b2959cc1b197b2096ba9c1a2e10b1d1ad4197d6
dmycse <dmycse@gmail.com>
2025-01-15 20:11:02 +0200

    Shared(api): add posts limit in one page

--- frontend/src/s-shared/api/posts/fetchPosts.js
+++ frontend/src/s-shared/api/posts/fetchPosts.js
@@ -4,7 +4,9 @@ export const fetchPosts = async (pageParam) => {
   return await res.json();
 };
 
+let postLimit = 3;
+
 export const fetchInfinityPosts = async (pageParam) => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/posts?page=${pageParam}`);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/posts?page=${pageParam}&limit=${postLimit}`);
   return await res.json();
 };
