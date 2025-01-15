3ef5272fac5af9cd470b79284a9eeaeb17a715c2
dmycse <dmycse@gmail.com>
2025-01-15 19:01:05 +0200

    Beackend: feat post routes

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { getAllPosts, getPost, createPost, deletePost } from "../controllers/post.controller.js";
+import { getAllPosts, getPost, createPost, deletePost, uploadAuth } from "../controllers/post.controller.js";
 
 
 const router = Router();
 
+router.get("/upload-auth", uploadAuth);
+
 router.get("/", getAllPosts);
 router.get("/:slug", getPost);
-
 router.post("/", createPost);
-
 router.delete("/:id", deletePost);
 
 
