06e817d9c6c05c0e0eeca5c456920fbe11ac3bdd
dmycse <dmycse@gmail.com>
2025-01-15 11:11:12 +0200

    Widget(categories): create CategoriesWidget component

---
+++ frontend/src/widgets/categories-widget/ui/CategoriesWidget.jsx
@@ -0,0 +1,12 @@
+import { CategoriesItems } from '@/entities/categories';
+import { SearchInput } from "@/shared/ui-elements/searchInput/SearchInput";
+
+export const CategoriesWidget = () => {
+  return (
+    <section className='p-4 shadow-lg hidden md:flex items-center justify-center gap-8 bg-white rounded-lg'>
+      <CategoriesItems   />
+      <span className='text-xl font-medium'>|</span>
+      <SearchInput />
+    </section>
+  )
+}
