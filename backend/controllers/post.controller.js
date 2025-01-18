278b6eec47658e859194ef026434dec1a036b8bf
dmycse <dmycse@gmail.com>
2025-01-18 12:00:43 +0200

    Backend: refactor post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -32,7 +32,7 @@ export const getAllPosts = async (req, res) => {
 };
 
 export const getPost = async (req, res) => {
-  const post = await Post.findOne( {slug: req.params.slug} );
+  const post = await Post.findOne( {slug: req.params.slug} ).populate('user', 'username img');
   res.status(200).json(post);
   
 };
@@ -60,7 +60,7 @@ export const createPost = async (req, res) => {
   const newPost = new Post({user: user._id, ...req.body, slug});
 
   const post = await newPost.save();
-  res.status(200).json(post);
+  res.status(201).json(post);
 };
 
 export const deletePost = async (req, res) => {
