b83abd1c7ac0b47289d7ed56d5e015c6498efac2
dmycse <dmycse@gmail.com>
2025-01-18 12:01:27 +0200

    Backend: create comment router

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { getPostsComments, createPostComment, removePostComment } from '../controllers/comment.controller.js';
 
 const router = Router();
 
-
+router.get('/:postId', getPostsComments);
+router.post('/:postId', createPostComment);
+router.delete('/:id', removePostComment);
 
 export default router;
