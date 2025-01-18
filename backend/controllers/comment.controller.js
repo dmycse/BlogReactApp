0f1820882459ef8ef2761d03984112b90c723706
dmycse <dmycse@gmail.com>
2025-01-18 12:01:46 +0200

    Backend: feature comment controller

---
+++ backend/controllers/comment.controller.js
@@ -0,0 +1,59 @@
+import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
+
+export const getPostsComments = async (req, res) => {
+
+  const comments = await Comment
+    .find({post: req.params.postId})
+    .populate('user', 'username img')
+    .sort({createdAt: -1})
+  
+  res.status(200).json(comments);
+};
+
+
+export const createPostComment = async (req, res) => {
+  const clerkUserId = req.auth.userId;
+ 
+  if (!clerkUserId) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  const user = await User.findOne({ clerkUserId });
+  
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const newComment = new Comment({
+    user: user._id,
+    post: req.params.postId,
+    ...req.body
+  });
+  
+  const comment = await newComment.save();
+ 
+  res.status(201).json(comment);
+};
+
+export const removePostComment = async (req, res) => {
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  const deletedComment = await Comment.findByIdAndDelete({
+    _id: req.params.id, 
+    user: user._id
+  });
+
+  if (!deletedPost) {
+    return res.status(403).json({ message: "You can't delete this comment" });
+  }
+  
+
+  res.status(200).json('Comment deleted');
+};
