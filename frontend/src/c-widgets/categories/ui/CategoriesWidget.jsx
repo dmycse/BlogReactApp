88743ac36add832989ee35c5f132f760accac13a
dmycse <dmycse@gmail.com>
2025-01-18 14:53:29 +0200

    Entities: refactor styles of categories items

--- frontend/src/c-widgets/categories/ui/CategoriesWidget.jsx
+++ frontend/src/c-widgets/categories/ui/CategoriesWidget.jsx
@@ -3,7 +3,7 @@ import { SearchInput } from "@/s-shared/ui/searchInput/SearchInput";
 
 export const CategoriesWidget = () => {
   return (
-    <section className='p-4 shadow-lg hidden md:flex items-center justify-center gap-8 bg-white rounded-lg'>
+    <section className='p-4 shadow-lg hidden md:flex items-center justify-center gap-6 bg-white rounded-lg'>
       <CategoriesItems   />
       <span className='text-xl font-medium'>|</span>
       <SearchInput />
