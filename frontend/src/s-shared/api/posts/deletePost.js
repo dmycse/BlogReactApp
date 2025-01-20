bcfc14ee6fa791d9f4b551dd9204f29452a69134
dmycse <dmycse@gmail.com>
2025-01-20 14:56:50 +0200

    Api: create user api

---
+++ frontend/src/s-shared/api/posts/deletePost.js
@@ -0,0 +1,10 @@
+export const deletePost = async (token, postId) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/posts/${postId}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return await res.json();
+};
--- frontend/src/s-shared/api/posts/fetchPosts.js
+++ frontend/src/s-shared/api/posts/fetchPosts.js
@@ -1,10 +1,10 @@
 
-export const fetchPosts = async (pageParam) => {
+export const fetchPosts = async () => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/posts`);
   return await res.json();
 };
 
-let postLimit = 3;
+let postLimit = 4;
 
 export const fetchInfinityPosts = async (pageParam) => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/posts?page=${pageParam}&limit=${postLimit}`);
