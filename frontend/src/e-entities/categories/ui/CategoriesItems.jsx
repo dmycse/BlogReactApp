e55d77a0442df1abc8ea10788a630365ad4507b0
dmycse <dmycse@gmail.com>
2025-01-20 14:54:48 +0200

    Categories: edit categories items styles

--- frontend/src/e-entities/categories/ui/CategoriesItems.jsx
+++ frontend/src/e-entities/categories/ui/CategoriesItems.jsx
@@ -3,11 +3,11 @@ import { categories } from '@/s-shared/constants';
 
 export const CategoriesItems = () => {
   return (
-    <ul className="flex-1 grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-y-3">
+    <ul className="flex-1 grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-y-3">
       {categories.map(item => (
         <li 
           key={item.id} 
-          className='py-2 m-auto first:bg-purple-500 first:text-white text-black rounded-lg
+          className='py-2 w-fit m-auto first:bg-purple-500 first:text-white text-black rounded-lg
                     hover:bg-purple-300 hover:text-white cursor-pointer'
         >
           <Link to={item.url} className='px-4'>
