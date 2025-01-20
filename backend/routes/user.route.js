83873494a585d1cad5f9e142baa68e9f59fef14e
dmycse <dmycse@gmail.com>
2025-01-20 14:56:24 +0200

    Backend: create user routes and user controller

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { getUserSavedPosts, savePost} from '../controllers/user.contoller.js';
 
 const router = Router();
 
-
+router.get('/saved', getUserSavedPosts);
+router.patch('/save', savePost);
 
 export default router;
