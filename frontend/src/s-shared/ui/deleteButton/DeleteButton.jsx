9e102d51a56d5d020567c6fb1eda893d7c772a36
dmycse <dmycse@gmail.com>
2025-01-25 11:14:24 +0200

    Shared(ui): feat DeleteButton component

--- frontend/src/s-shared/ui/deleteButton/DeleteButton.jsx
+++ frontend/src/s-shared/ui/deleteButton/DeleteButton.jsx
@@ -1,9 +1,10 @@
 
 
-export const DeleteButton = ({width, height, onDeleteSubmit, children}) => {
+export const DeleteButton = ({width, height, onDeleteSubmit, disabled, children}) => {
   return (
     <button
       className="py-1 flex items-center gap-2 text-sm cursor-pointer hover:text-red-700"
+      disabled={disabled}
       onClick={onDeleteSubmit}
     >
       <svg
