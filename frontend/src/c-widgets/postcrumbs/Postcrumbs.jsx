8157174f34e42004ca4ddc356de79efd92ca2f0e
dmycse <dmycse@gmail.com>
2025-01-26 11:49:24 +0200

    Widgets(postcrumbs): edit link in Postcrumbs component

--- frontend/src/c-widgets/postcrumbs/Postcrumbs.jsx
+++ frontend/src/c-widgets/postcrumbs/Postcrumbs.jsx
@@ -5,7 +5,7 @@ export const Postcrubms = ({author, category, date}) => {
   return (
     <div className="flex items-center gap-2 text-sm text-gray-400">
       <span>Written by</span>
-      <Link to='' className="text-purple-500">{author}</Link>
+      <Link to={`/posts?author=${author}`} className="text-purple-500">{author}</Link>
       <span>on</span>
       <Link to={`/posts?category=${category}`} className="text-purple-500">{category}</Link>
       <span>{date}</span>
