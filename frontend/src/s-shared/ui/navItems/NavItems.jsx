0f1a19b217d531cc1736df01964beb5610b5ebcf
dmycse <dmycse@gmail.com>
2025-01-26 11:25:53 +0200

    Shared(ui): refactor NavItem component

--- frontend/src/s-shared/ui/navItems/NavItems.jsx
+++ frontend/src/s-shared/ui/navItems/NavItems.jsx
@@ -9,7 +9,7 @@ export const NavItems = () => {
           key={item.id} 
         >
           <Link 
-            href={item.url}
+            to={item.url}
             className="px-5 py-2 hover:bg-purple-300 hover:text-white rounded-lg"
           >
             {item.title}
