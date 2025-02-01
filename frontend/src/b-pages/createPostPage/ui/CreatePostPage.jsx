7787fcfc98c8031970fb23aa3b14ee9ed4ac9d38
dmycse <dmycse@gmail.com>
2025-02-01 12:42:16 +0200

    Pages: add logic to return to CreatePostPage from LoginPage after login

--- frontend/src/b-pages/createPostPage/ui/CreatePostPage.jsx
+++ frontend/src/b-pages/createPostPage/ui/CreatePostPage.jsx
@@ -1,6 +1,19 @@
+import { Link, useLocation } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 import { CreatePostForm } from "@/d-features/createPost";
 
 export const CreatePostPage = () => {
+  const { isSignedIn } = useUser()
+  const location = useLocation();
+ 
+  if (!isSignedIn) {
+    return (
+      <div className="inline-flex gap-2">
+        <span className="text-red-400">To create a post, you must be logged in: </span>
+        <Link to="/login" state={{ from: location.pathname }} className="text-blue-600">Login</Link>
+      </div>
+    )
+  }
 
   return (
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
--- frontend/src/b-pages/loginPage/ui/LoginPage.jsx
+++ frontend/src/b-pages/loginPage/ui/LoginPage.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import { useLocation } from "react-router-dom";
 import { SignIn } from '@clerk/clerk-react';
 
 export const LoginPage = () => {
+  const location = useLocation();
+  const [locationPath, setLocationPath] = useState(location.state?.from || "/");
+
   return (
     <div className='h-[calc(100vh-4rem)] flex items-center justify-center'>
-      <SignIn signUpUrl='/register' />
+      <SignIn 
+        signUpUrl='/register' 
+        forceRedirectUrl={locationPath} 
+        // appearance={{
+        //   elements: {
+        //     footerAction: { display: "none"  },
+        //   },
+        // }} 
+      />
     </div>
   )
 }
