82f83769675839e0cccaa9b12a0c9e2fc37d4fa0
dmycse <dmycse@gmail.com>
2025-02-01 20:23:43 +0200

    Backend: feractor connectDB for serverless-friendly connection

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,12 +1,24 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log('\x1b[42m%s\x1b[0m', " MongoDB is connected ");
-  } catch (error) {
-    console.log(error);
-  }
+const MONGODB_URI = process.env.MONGODB_URL;
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(MONGODB_URI);
+//     console.log('\x1b[42m%s\x1b[0m', " MongoDB is connected ");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export default connectDB;
+
+// efficient MongoDB connection (serverless-friendly):
+if (!global.mongooseConnection) {
+  global.mongooseConnection = mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
-export default connectDB;
+export default global.mongooseConnection;
