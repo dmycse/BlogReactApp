1948ff9306b4181a8896a06cb6c261c0284e66d1
dmycse <dmycse@gmail.com>
2025-02-01 12:27:30 +0200

    Shared(lib): remove logs from useSearchInput hook

--- frontend/src/s-shared/lib/useSearchInput.jsx
+++ frontend/src/s-shared/lib/useSearchInput.jsx
@@ -4,23 +4,21 @@ import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 export const useSearchInput = () => {
 
   const location = useLocation();
-    const navigate = useNavigate();
-    const [searchParams, setSearchParams] = useSearchParams();
-    const inputRef = useRef(null);
-    console.log('Location', location);
-    console.log('SearchParams Object', Object.fromEntries(searchParams));
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const inputRef = useRef(null);
 
-    const handleKeyDown = (e) => {
-      if (e.key === 'Enter') {
-        const query = e.target.value;
-        if (location.pathname === '/posts') {
-          setSearchParams({...Object.fromEntries(searchParams), search: query});
-          inputRef.current.value = '';
-        } else {
-          navigate(`/posts?search=${query}`);
-        }
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      const query = e.target.value;
+      if (location.pathname === '/posts') {
+        setSearchParams({...Object.fromEntries(searchParams), search: query});
+        inputRef.current.value = '';
+      } else {
+        navigate(`/posts?search=${query}`);
       }
-    };
+    }
+  };
 
   return {
     inputRef,
