5a5ce48c7cf5edbe38c85f69afccc059c33574c6
dmycse <dmycse@gmail.com>
2025-02-01 12:35:48 +0200

    Featured: feat CreateComment component

--- frontend/src/d-features/createComment/ui/CreateComment.jsx
+++ frontend/src/d-features/createComment/ui/CreateComment.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 import { useCreateComment } from '../lib/useCreateComment';
 import DOMPurify from 'dompurify';
 
 export const CreateComment = ({ postId }) => {
 
+  const { user } = useUser();
+  
   let { createNewComment, isPending, error } = useCreateComment(postId);
   
   const handleSubmit = (e) => {
@@ -22,21 +26,38 @@ export const CreateComment = ({ postId }) => {
   };
 
   return (
-    <form 
-      className="mb-8 w-full flex items-center justify-between gap-8"
-      onSubmit={handleSubmit} 
-    >
-      <textarea 
-        name="content" 
-        id="content" 
-        placeholder='Write a comment' 
-        className='p-3 w-full outline-none rounded-xl'
-      />
-      <button 
-        className='px-4 py-2 w-32 bg-purple-500 text-white rounded-xl'
+    <>
+      {!user && (
+          <div>
+            <Link 
+              to='/login'
+              state={{ from: location.pathname }} 
+              className="text-sm"
+            >
+              <span className="text-blue-800 font-semibold">Login</span>
+              <span className="text-red-400"> to create a comment</span>
+            </Link>
+          </div>
+        )}
+      <form 
+        className="mb-8 w-full flex items-center justify-between gap-8"
+        onSubmit={handleSubmit} 
       >
-        {isPending ? "Sending..." : "Send"}
-      </button>
-    </form>
+        <textarea 
+          name="content" 
+          id="content" 
+          placeholder='Write a comment'
+          disabled={isPending || !user} 
+          className='p-3 w-full outline-none rounded-xl'
+        />
+        <button
+          type="submit"
+          disabled={isPending || !user} 
+          className='px-4 py-2 w-32 bg-purple-500 text-white rounded-xl disabled:bg-slate-400 disabled:text-slate-300'
+        >
+          {isPending ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </>
   )
 }
